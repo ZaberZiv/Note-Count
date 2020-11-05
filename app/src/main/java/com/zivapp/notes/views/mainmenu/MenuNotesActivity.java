8a00ebc8b44b1c41ee1bf38138300be0b568f72d
Ilya Zaberezhnyy <izaber@list.ru>
2020-11-05 21:58:34 +0600

    menu spinner added

--- app/src/main/java/com/zivapp/notes/views/mainmenu/MenuNotesActivity.java
+++ app/src/main/java/com/zivapp/notes/views/mainmenu/MenuNotesActivity.java
@@ -1,8 +1,18 @@
 package com.zivapp.notes.views.mainmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.zivapp.notes.R;
+import com.zivapp.notes.views.login.LoginActivity;
 
 public class MenuNotesActivity extends AppCompatActivity {
 
@@ -14,5 +24,40 @@ public class MenuNotesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu_notes);
 
         new MenuPresenter(this, this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.v(TAG, "onOptionsItemSelected()");
+
+        if (item.getItemId() == R.id.sign_out) {
+            button();
+            Log.v(TAG, "sign_out");
+
+            return true;
+        }
+        Log.v(TAG, "onOptionsItemSelected");
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void button() {
+        Log.v(TAG, "button");
+
+        findViewById(R.id.sign_out).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "User signed out!");
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MenuNotesActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
