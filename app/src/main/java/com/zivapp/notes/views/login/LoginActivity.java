7661cd128a277fc35a9851292df9cda057e14264
Ilya <izaber@list.ru>
2020-11-20 16:14:07 +0600

    animation added

--- app/src/main/java/com/zivapp/notes/views/login/LoginActivity.java
+++ app/src/main/java/com/zivapp/notes/views/login/LoginActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.zivapp.notes.R;
 import com.zivapp.notes.databinding.ActivityLoginBinding;
@@ -31,9 +33,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.sign_in_btn:
+                animateTextSize(mBinding.signInBtn, 28, 42);
+                animateTextSize(mBinding.registerBtn, 42, 28);
                 setFragment(new LoginFragment());
                 break;
             case R.id.register_btn:
+                animateTextSize(mBinding.signInBtn, 42, 28);
+                animateTextSize(mBinding.registerBtn, 28, 42);
                 setFragment(new RegisterFragment());
         }
     }
@@ -41,5 +47,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void setFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 fragment).commit();
+    }
+
+    private void animateTextSize(final TextView tv, float startSize, float endSize) {
+        long animationDuration = 600; // Animation duration in ms
+
+        ValueAnimator animator = ValueAnimator.ofFloat(startSize, endSize);
+        animator.setDuration(animationDuration);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float animatedValue = (float) valueAnimator.getAnimatedValue();
+                tv.setTextSize(animatedValue);
+            }
+        });
+
+        animator.start();
     }
 }
